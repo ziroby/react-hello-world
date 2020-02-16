9d65dd1b3f15f8fe2924fa5bb5cf16fbe118ab60
Ron Ziroby Romero <ziroby@ziroby.com>
2020-02-16 14:07:24 +0000

    Add HelloWorld component to main screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HelloWorld from "./component/hello-world/hello-world.component";
+
 function App() {
   return (
     <div className="App">
@@ -10,15 +12,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	  </header>
+	  <p><HelloWorld /></p>
     </div>
   );
 }
